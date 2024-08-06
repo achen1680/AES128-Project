1e0381a4bfae57dea7be5f7f21e7f554ddec9a05
achen1680 <116025123+achen1680@users.noreply.github.com>
2024-08-06 18:59:31 -0400

    Add files via upload

--- gen_key.v
+++ gen_key.v
@@ -4,18 +4,20 @@ module gen_key (
     output [127:0] key_out
 );
 
-wire [31:0] col1, col2, col3, col4, temp;
+wire [31:0] col1, col2, col3, col4, temp, rotated_col4;
 
 assign col4 = key_in[31:0];
 assign col3 = key_in[63:32];
 assign col2 = key_in[95:64];
 assign col1 = key_in[127:96];
+assign rotated_col4 = rotate(col4);
+
 
 // sbox function takes a rotated verison of last column of the previous key and stores it in temp
-sbox first(.col(rotate(col4)[31:24]), .subbed(temp[31:24]));
-sbox second(.col(rotate(col4)[23:16]), .subbed(temp[23:16]));
-sbox third(.col(rotate(col4)[15:8]), .subbed(temp[15:8]));
-sbox fourth(.col(rotate(col4)[7:0]), .subbed(temp[7:0]));
+sbox first(.col(rotated_col4[31:24]), .subbed(temp[31:24]));
+sbox second(.col(rotated_col4[23:16]), .subbed(temp[23:16]));
+sbox third(.col(rotated_col4[15:8]), .subbed(temp[15:8]));
+sbox fourth(.col(rotated_col4[7:0]), .subbed(temp[7:0]));
 
 //xor with 1st col of previous round key and then xor with rcon
 assign key_out[127:96] = col1 ^ temp ^ rcon(round);
